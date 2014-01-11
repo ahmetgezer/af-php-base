34ab4dc203301493e2e9146fc01484c8bd2c7adc
ahmetgezer <benahmetgezer@gmail.com>
2014-01-11 23:52:02 +0200

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory --> <div id="fb-root"></div>
+    <div id="fb-root"></div><script src="https://connect.facebook.net/en_US/all.js#appId=127182060688835&xfbml=1"></script> 
+    <script type="text/javascript">
+
+    FB.init({
+        appId: '469960896443248', 
+        status: true, 
+        cookie: true, 
+        xfbml: true
+    });
+
+    /* Init fb Resize window */
+
+    /* Completly removed now in JS SDK*/
+    /* FB.Canvas.setAutoResize(7); */
+
+    /* As of Jan 2012 you need to use */
+    FB.Canvas.setAutoGrow(7);
+
+    </script>
+
+
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+      
+       
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+       <div id="site">
+   
+         <div id="main">
+               <div id="logo"><img src="img/logo.png" width="149" height="121"></div>
+           <div class="neden">
+           <p>Ohannes Burger Facebook sayfasına hoş geldin!</p>  
+           
+           <p>Ohannes Burger ile ilgili görüş ve önerilerini burada paylaşabilir, sevdiğin uygulama ve içeriklerimizi beğenebilirken, sevmediklerin hakkında da geribildirim verebilirsin. Unutma, senden gelecek her türlü görüş ve öneriye açığız. Fakat sayfamızın kuralları gereğince:<br>
+           
+           <p>•	Küfür, hakaret, aşağılama ve kötü üslup içeren<br>
+•	Markamızla alakasız reklam içeren<br>
+•	Yasal olmayan, kanunlara ve düzenlemelere aykırı<br>
+•	Kişi haklarına yönelik saldırıda bulunan<br>
+•	Facebook kurallarında belirtilen maddelere uymayan<br>
+<br>
+
+Yorumlar yazmamaya özen göstermeni rica ederiz. Bu tür yorum ve paylaşımlar Ohannes Burger sayfa yöneticileri tarafından silinecek veya kullanıcıların sayfaya erişimleri engellenecektir.<br><br>
+Ohannes Burger, sayfa yöneticileri dışında bir kişi ya da kişilerce yazılmış yorumlar ve paylaşılmış içeriklerden sorumlu değildir.</p>
+
+           
+           
+           
+           
+           
+           
+           
+           
+           
+             </div>  
+           <footer>
+            <div class="logos"><img src="img/logos.png" width="55" height="46"  alt=""/></div>
+             <div class="footernote">www.ohannesburger.com    |   0 232 373 9970</div>
+           </footer> 
+              
+         </div></div>
+               
+
+
+   
+    </body>
+</html>
